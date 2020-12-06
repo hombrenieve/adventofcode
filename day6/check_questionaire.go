29295d23abe012be18de9b0d2305d1f4346d5915
Hombrenieve <hombrenieve@gmail.com>
2020-12-06 16:53:18 +0100

    Day6 exercise 2 a bit of functional

--- day6/check_questionaire.go
+++ day6/check_questionaire.go
@@ -6,9 +6,17 @@ import (
 	"os"
 )
 
+func filter(collection map[byte]int, pred func(byte, int) bool) map[byte]int {
+	for k, v := range collection {
+		if !pred(k, v) {
+			delete(collection, k)
+		}
+	}
+	return collection
+}
+
 func calculateNumberOfValidQuestions(questionaire string, members int) int {
 	validQuestions := make(map[byte]int)
-	var valids int
 	for _, q := range questionaire {
 		bq := byte(q)
 		if _, ok := validQuestions[bq]; ok {
@@ -16,11 +24,10 @@ func calculateNumberOfValidQuestions(questionaire string, members int) int {
 		} else {
 			validQuestions[bq] = 1
 		}
-		if validQuestions[bq] == members {
-			valids++
-		}
 	}
-	return valids
+	return len(filter(validQuestions, func(_ byte, v int) bool {
+		return v == members
+	}))
 }
 
 func main() {
