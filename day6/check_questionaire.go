81b72a570f4c2f484e3252709865ad6eb03809ec
Hombrenieve <hombrenieve@gmail.com>
2020-12-06 16:32:20 +0100

    Day6 exercise 2

--- day6/check_questionaire.go
+++ day6/check_questionaire.go
@@ -6,26 +6,38 @@ import (
 	"os"
 )
 
-func calculateNumberOfValidQuestions(questionaire string) int {
-	validQuestions := make(map[byte]bool)
+func calculateNumberOfValidQuestions(questionaire string, members int) int {
+	validQuestions := make(map[byte]int)
+	var valids int
 	for _, q := range questionaire {
-		validQuestions[byte(q)] = true
+		bq := byte(q)
+		if _, ok := validQuestions[bq]; ok {
+			validQuestions[bq]++
+		} else {
+			validQuestions[bq] = 1
+		}
+		if validQuestions[bq] == members {
+			valids++
+		}
 	}
-	return len(validQuestions)
+	return valids
 }
 
 func main() {
 	file, _ := os.Open("input")
 	scanner := bufio.NewScanner(file)
 	var groupQuestionaire string
+	var members int
 	var validQuestions int
 	for scanner.Scan() {
 		buff := scanner.Text()
 		if len(buff) != 0 {
 			groupQuestionaire = groupQuestionaire + buff
+			members++
 		} else {
-			validQuestions += calculateNumberOfValidQuestions(groupQuestionaire)
+			validQuestions += calculateNumberOfValidQuestions(groupQuestionaire, members)
 			groupQuestionaire = ""
+			members = 0
 		}
 	}
 	fmt.Println(validQuestions)
