3f73a3c4b2f0a29786c881f0197709797ca31e8e
Hombrenieve <hombrenieve@gmail.com>
2020-12-08 15:24:40 +0100

    Day8 exercise 2

--- day8/check_progrm.go
+++ day8/check_progrm.go
@@ -14,21 +14,39 @@ type instruction struct {
 	executed bool
 }
 
-func executeUntilLoop(program []instruction, pi int, acc int) int {
+func executeUntilLoop(program []instruction, pi int, acc int, changed bool) (int, bool) {
+	if pi == len(program) {
+		fmt.Printf("Reach end of program at %d\n", pi)
+		return acc, true
+	}
+	fmt.Printf("Executing %v with acc: %d\n", program[pi], acc)
 	if program[pi].executed {
-		return acc
+		fmt.Println("Loop detected")
+		return -1, false
 	}
 	program[pi].executed = true
+	var accRet int
+	var status bool
 	switch program[pi].cmd {
 	case "nop":
-		return executeUntilLoop(program, pi+1, acc)
+		accRet, status = executeUntilLoop(program, pi+1, acc, changed)
+		if !status && !changed {
+			accRet, status = executeUntilLoop(program, pi+program[pi].arg, acc, true)
+		}
 	case "acc":
-		return executeUntilLoop(program, pi+1, acc+program[pi].arg)
+		accRet, status = executeUntilLoop(program, pi+1, acc+program[pi].arg, changed)
 	case "jmp":
-		return executeUntilLoop(program, pi+program[pi].arg, acc)
+		accRet, status = executeUntilLoop(program, pi+program[pi].arg, acc, changed)
+		if !status && !changed {
+			accRet, status = executeUntilLoop(program, pi+1, acc, true)
+		}
 	}
-	fmt.Println("PROGRAM ERROR")
-	return -1
+	if !status {
+		program[pi].executed = false
+		fmt.Printf("Unvisiting %v\n", program[pi])
+		return -1, false
+	}
+	return accRet, status
 }
 
 func main() {
@@ -42,5 +60,6 @@ func main() {
 		arg, _ := strconv.Atoi(inst[1])
 		program = append(program, instruction{cmd, arg, false})
 	}
-	fmt.Printf("Program looped: acc: %v\n", executeUntilLoop(program, 0, 0))
+	acc, status := executeUntilLoop(program, 0, 0, false)
+	fmt.Printf("Program status: %v acc: %v\n", status, acc)
 }
