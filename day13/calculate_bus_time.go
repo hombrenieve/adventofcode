849501081e2b080213b8ab51cec70dda81b31d10
Hombrenieve <hombrenieve@gmail.com>
2020-12-13 23:37:11 +0100

    Day13 exercise 2

--- day13/calculate_bus_time.go
+++ day13/calculate_bus_time.go
@@ -20,21 +20,62 @@ func busesIdsConvert(busesS []string) (map[int]int, int) {
 		}
 	}
 	sort.Ints(sorted)
-	return buses, sorted[len(sorted)-1]
+	return buses, sorted[0]
 }
 
-func checkConditions(t int64, buses map[int]int) bool {
-	for id, offset := range buses {
-		if (t+int64(offset))%int64(id) != 0 {
+var agreed []int
+var interval int64
+
+func isContained(a int, b []int) bool {
+	for _, v := range b {
+		if a == v {
+			return true
+		}
+	}
+	return false
+}
+
+func contains(a, b []int) bool {
+	for _, vb := range b {
+		if !isContained(vb, a) {
 			return false
 		}
 	}
 	return true
 }
 
-func calculateBusTime(buses map[int]int, biggest int) int64 {
-	t := int64(biggest - buses[biggest])
-	for ; !checkConditions(t, buses); t += int64(biggest) {
+func multAll(b []int) int64 {
+	var m int64 = 1
+	for _, v := range b {
+		m *= int64(v)
+	}
+	return m
+}
+
+func checkConditions(t int64, buses map[int]int) bool {
+	status := true
+	var agreedNow []int
+	for id, offset := range buses {
+		if (t+int64(offset))%int64(id) != 0 {
+			status = false
+		} else {
+			agreedNow = append(agreedNow, id)
+		}
+	}
+	if len(agreedNow) > 1 {
+		if len(agreedNow) > len(agreed) && contains(agreedNow, agreed) {
+			interval = multAll(agreedNow)
+			fmt.Println("New interval found at", t, "Numbers:", agreedNow, "Interval:", interval)
+			agreed = agreedNow
+		}
+	}
+	return status
+}
+
+func calculateBusTime(buses map[int]int, smallest int) int64 {
+	interval = int64(smallest)
+	t := int64(smallest)
+	for ; !checkConditions(t, buses); t += interval {
 		//fmt.Println("Checking:", t)
 	}
 	return t
@@ -45,7 +86,7 @@ func main() {
 	scanner := bufio.NewScanner(file)
 	scanner.Scan()
 	scanner.Scan()
-	busesIds, biggest := busesIdsConvert(strings.Split(scanner.Text(), ","))
-	fmt.Println("Ids:", busesIds, "Biggest:", biggest)
-	fmt.Println("Timestamp:", calculateBusTime(busesIds, biggest))
+	busesIds, smallest := busesIdsConvert(strings.Split(scanner.Text(), ","))
+	fmt.Println("Ids:", busesIds, "Biggest:", smallest)
+	fmt.Println("Timestamp:", calculateBusTime(busesIds, smallest))
 }
