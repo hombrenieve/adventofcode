ac27be8542c12f70ae8fc7dc1c664c54ebd48378
Hombrenieve <hombrenieve@gmail.com>
2020-12-14 23:43:39 +0100

    Day10 exercise 2
    After days Lau had the idea... divide and conquer

--- day10/count_adapter_chains.go
+++ day10/count_adapter_chains.go
@@ -12,7 +12,7 @@ func canIRemove(input []int) bool {
 	return input[2]-input[0] < 4
 }
 
-func countOptions(input []int) int {
+func countOptions(input []int) int64 {
 	if len(input) == 2 {
 		return 1
 	}
@@ -23,11 +23,29 @@ func countOptions(input []int) int {
 	return countOptions(input[1:])
 }
 
-func getCombinations(input []int) int {
+func divideAndCalculate(input []int) int64 {
+	next := 0
+	var results []int64
+	for i := 0; i < len(input); i++ {
+		if !canIRemove(input[i : i+3]) {
+			if next != i {
+				results = append(results, countOptions(input[next:i+2]))
+			}
+			next = i + 1
+		}
+	}
+	var result int64 = 1
+	for _, v := range results {
+		result *= int64(v)
+	}
+	return result
+}
+
+func getCombinations(input []int) int64 {
 	sort.Ints(input)
 	newInput := append([]int{0}, input...)
 	newInput = append(newInput, input[len(input)-1]+3)
-	return countOptions(newInput)
+	return divideAndCalculate(newInput)
 }
 
 func main() {
