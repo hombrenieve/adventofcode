4bed86df57b23a394377d79dfebe292f88bd80c1
Hombrenieve <hombrenieve@gmail.com>
2020-12-15 13:04:53 +0100

    Day15 exercise 1

---
+++ day15/play_number_game.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+type gameStatus struct {
+	lastTurn     int
+	previousTurn int
+}
+
+func main() {
+	gameMap := make(map[int]*gameStatus)
+	gameTurn := 1
+	lastNumber := 0
+	for _, numbS := range strings.Split(os.Args[1], ",") {
+		numb, _ := strconv.Atoi(numbS)
+		gameMap[numb] = &gameStatus{gameTurn, -1}
+		fmt.Println("Turn:", gameTurn, "Number:", numb)
+		gameTurn++
+		lastNumber = numb
+	}
+	targetTurn, _ := strconv.Atoi(os.Args[2])
+	for ; gameTurn <= targetTurn; gameTurn++ {
+		if status, ok := gameMap[lastNumber]; !ok {
+			gameMap[lastNumber] = &gameStatus{gameTurn - 1, -1}
+			lastNumber = 0
+		} else {
+			status.previousTurn = status.lastTurn
+			status.lastTurn = gameTurn - 1
+			lastNumber = status.lastTurn - status.previousTurn
+		}
+		fmt.Println("Turn:", gameTurn, "Number:", lastNumber)
+	}
+}
