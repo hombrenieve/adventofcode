c176c252fbace27ca948bc5417386e0a00545bd7
Hombrenieve <hombrenieve@gmail.com>
2020-12-25 23:27:32 +0100

    Day19 with regexp

--- day19/monster_message.go
+++ day19/monster_message.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"regexp"
 	"strconv"
 	"strings"
 )
@@ -12,34 +13,27 @@ type rule struct {
 	parts [2][]int
 }
 
-func (r rule) expand() []string {
-	var theExpanse []string
+func (r rule) regex(rm *ruleMap) string {
+	regex := "("
 	for i := 0; i < 2; i++ {
-		var currentExpanse []string
+		var currentRegex string
 		for _, rl := range r.parts[i] {
-			if rl == ruleMap.ruleA {
-				currentExpanse = mergeStrings(currentExpanse, []string{"a"})
-			} else if rl == ruleMap.ruleB {
-				currentExpanse = mergeStrings(currentExpanse, []string{"b"})
+			if rl == rm.ruleA {
+				currentRegex += "a"
+			} else if rl == rm.ruleB {
+				currentRegex += "b"
 			} else {
-				currentExpanse = mergeStrings(currentExpanse, ruleMap.theMap[rl].expand())
+				currentRegex += rm.theMap[rl].regex(rm)
 			}
 		}
-		theExpanse = append(theExpanse, currentExpanse...)
-	}
-	return theExpanse
-}
-
-func mergeStrings(current []string, news []string) (merged []string) {
-	if len(current) == 0 {
-		return news
-	}
-	for _, c := range current {
-		for _, n := range news {
-			merged = append(merged, c+n)
+		if i == 0 && len(r.parts[1]) > 0 {
+			regex += currentRegex + "|"
+		} else {
+			regex += currentRegex
 		}
 	}
-	return merged
+	regex += ")"
+	return regex
 }
 
 func createRule(parts string) rule {
@@ -56,21 +50,32 @@ func createRule(parts string) rule {
 	return theRule
 }
 
-var ruleMap struct {
+type ruleMap struct {
 	theMap       map[int]rule
 	ruleA, ruleB int
+	compiled     *regexp.Regexp
 }
 
-var expandedRules map[string]bool
+func createRuleMap() *ruleMap {
+	var rm ruleMap
+	rm.theMap = make(map[int]rule)
+	return &rm
+}
 
-func check(message string) bool {
-	_, ok := expandedRules[message]
-	return ok
+func (rm *ruleMap) buildRegexp() {
+	regex := "^" + rm.theMap[0].regex(rm) + "$"
+	fmt.Println("Regex:", regex)
+	rm.compiled = regexp.MustCompile(regex)
+
+}
+
+func (rm *ruleMap) check(message string) bool {
+	return rm.compiled.MatchString(message)
 }
 
 func main() {
 	file, _ := os.Open("input")
-	ruleMap.theMap = make(map[int]rule)
+	rm := createRuleMap()
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		line := scanner.Text()
@@ -81,26 +86,20 @@ func main() {
 		ruleNr, _ := strconv.Atoi(lineS[0])
 		rulePart := strings.Trim(lineS[1], " ")
 		if rulePart == "\"a\"" {
-			ruleMap.ruleA = ruleNr
+			rm.ruleA = ruleNr
 			continue
 		} else if rulePart == "\"b\"" {
-			ruleMap.ruleB = ruleNr
+			rm.ruleB = ruleNr
 			continue
 		}
 		newRule := createRule(rulePart)
-		ruleMap.theMap[ruleNr] = newRule
+		rm.theMap[ruleNr] = newRule
 	}
-	fmt.Println("Rule for A:", ruleMap.ruleA, "Rule for B:", ruleMap.ruleB)
-	fmt.Println(ruleMap)
-	expandedRules = make(map[string]bool)
-	expanse := ruleMap.theMap[0].expand()
-	for _, k := range expanse {
-		expandedRules[k] = true
-	}
-	fmt.Println("Expanded:", len(expandedRules))
+	fmt.Println("Rule for A:", rm.ruleA, "Rule for B:", rm.ruleB)
+	rm.buildRegexp()
 	var matched int
 	for scanner.Scan() {
-		if check(scanner.Text()) {
+		if rm.check(scanner.Text()) {
 			matched++
 		}
 	}
