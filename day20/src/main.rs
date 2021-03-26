3249abb8bdcc30970fb860e089642b4fd017e323
Hombrenieve <hombrenieve@gmail.com>
2021-03-26 01:33:41 +0100

    Start reading file
    Last tile is missing

---
+++ day20/src/main.rs
@@ -0,0 +1,41 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+
+struct Tile {
+    id: u32,
+    data: Vec<String>
+}
+
+fn main() {
+    let lines = read_lines("./input").unwrap();
+    let mut tile_lines: Vec<String> = vec![];
+    for line in lines {
+        if let Ok(line) = line {
+            if line == "" {
+                let tile = build_tile(tile_lines);
+                println!("Tile: {}", tile.id);
+                tile_lines = vec![];
+            } else {
+                tile_lines.push(line)
+            }
+        }
+    }
+}
+
+// The output is wrapped in a Result to allow matching on errors
+// Returns an Iterator to the Reader of the lines of the file.
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn build_tile(lines: Vec<String>) -> Tile
+{    
+    let title = lines[0].as_str().trim_right_matches(":").to_string().split(" ").nth(1).unwrap().to_string().parse::<u32>().unwrap();
+    let mut tile = Tile{id: title, data: vec![]};
+    tile
+}
+
