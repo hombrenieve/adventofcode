d3c3c5cb8b3746c700f2c8530433133214c2fc92
Hombrenieve <hombrenieve@gmail.com>
2021-03-28 17:27:44 +0200

    Adding modifiying operations to tile

--- day20/src/main.rs
+++ day20/src/main.rs
@@ -5,7 +5,36 @@ use std::path::Path;
 #[derive(Debug)]
 struct Tile {
     id: u32,
-    data: Vec<String>
+    data: Vec<String>,
+    //rotation, flip vertical, flip horizontal
+    status: (u16, bool, bool)
+}
+
+impl Tile {
+    fn new(lines: Vec<String>) -> Tile
+    {    
+        let title = lines[0].as_str().trim_end_matches(":").to_string().split(" ").nth(1).unwrap().to_string().parse::<u32>().unwrap();
+        Tile{id: title, data: lines[1..].iter().cloned().collect(), status: (0, false, false)}
+    }
+    fn rotate(&mut self) {
+        let mut new_data: Vec<String> = vec![];
+        for pos in 0..self.data[0].len() {
+            let mut line = String::new();
+            for i in (0..self.data.len()).rev() {
+                line.push(self.data[i].chars().nth(pos).unwrap())
+            }
+            new_data.push(line);
+        }
+        self.data = new_data;
+        self.status.0 = (self.status.0+90) % 360;
+    }
+    fn flipv(&mut self) {
+        self.data.reverse();
+        self.status.1 = !self.status.1;
+    }
+    fn fliph(&mut self) {
+        //self.data.iter_mut().map(|s| s.reverse());
+    }
 }
 
 fn main() {
@@ -15,7 +44,7 @@ fn main() {
     for line in lines {
         if let Ok(line) = line {
             if line == "" {
-                let tile = build_tile(tile_lines);
+                let tile = Tile::new(tile_lines);
                 println!("Tile: {:?}", tile);
                 tiles.push(tile);
                 tile_lines = vec![];
@@ -23,6 +52,14 @@ fn main() {
                 tile_lines.push(line)
             }
         }
+    }
+    for _ in 0..4 {
+        tiles[0].rotate();
+        println!("Rotado: {:?}", tiles[0]);
+    }
+    for _ in 0..2 {
+        tiles[0].flipv();
+        println!("Flipv: {:?}", tiles[0]);
     }
 }
 
@@ -34,11 +71,5 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
-fn build_tile(lines: Vec<String>) -> Tile
-{    
-    let title = lines[0].as_str().trim_right_matches(":").to_string().split(" ").nth(1).unwrap().to_string().parse::<u32>().unwrap();
-    let mut tile = Tile{id: title, data: vec![]};
-    tile.data.extend_from_slice(&lines[1..]);
-    tile
-}
+
 
