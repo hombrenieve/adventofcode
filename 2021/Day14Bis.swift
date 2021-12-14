e01db61ec79719a3e833a1bf44440014c0ded39c
Hombrenieve <hombrenieve@gmail.com>
2021-12-15 00:21:28 +0100

    Still not running part 2

--- 2021/Day14Bis.swift
+++ 2021/Day14Bis.swift
@@ -1,56 +1,73 @@
 import Foundation
 
+func quicksort<T: Comparable>(_ a: [T]) -> [T] {
+    guard a.count > 1 else { return a }
 
-var rules: Dictionary<String, String> = [:]
+    let pivot = a[a.count/2]
+    let less = a.filter { $0 < pivot }
+    let equal = a.filter { $0 == pivot }
+    let greater = a.filter { $0 > pivot }
 
-class RuleGraph {
-    let head: RuleGraph?
+    return quicksort(less) + equal + quicksort(greater)
+}
+
+typealias Counter = Dictionary<Character, Int>
+var rules: Dictionary<String, RuleGraph> = [:]
+
+extension Counter {
+    func merge(_ rhs: Counter) -> Counter {
+        var result = self
+        for (k, v) in rhs {
+            if let exist = self[k] {
+                result[k] = exist + v
+            } else {
+                result[k] = v
+            }
+        }
+        return result
+    }
+}
+
+class RuleGraph: Equatable {
     let pair: String
+    let insert: String
     var left: RuleGraph?
     var right: RuleGraph?
 
-    init(_ pair: String, head: RuleGraph?) {
+    init(_ pair: String, _ insert: String) {
         self.pair = pair
-        self.head = head
-        let names = self.generate()
-        if head == nil {
-            self.left = RuleGraph(names.0, head: self)
-            self.right = RuleGraph(names.1, head: self)
-        } else {
-            if let left = head!.find(names.0) {
-                self.left = left
-            } else {
-                self.left = RuleGraph(names.0, head: head)
-            }
-            if let right = head!.find(names.1) {
-                self.right = right
-            } else {
-                self.right = RuleGraph(names.1, head: head)
-            }
-        }
-    }
-    
-    private func generate() -> (String, String) {
-        let ins = rules[pair]!
-        return (String(pair.first!)+ins, ins+String(pair.last!))
+        self.insert = insert
     }
 
-    func find(_ elem: String) -> RuleGraph? {
-        if elem == self.pair {
-            return self
-        }
-        if let left = self.left {
-            if let found = left.find(elem) {
-                return found
-            }
-        }
-        if let right = self.right {
-            if let found = right.find(elem) {
-                return found
-            }
-        }
-        return nil
+    static func ==(lhs: RuleGraph, rhs: RuleGraph) -> Bool {
+        return lhs.pair == rhs.pair
     }
+    
+    func generate() -> (String, String) {
+        return (String(pair.first!)+insert, insert+String(pair.last!))
+    }
+
+    private func countLocalLetters() -> Counter {
+        var result: Counter = [:]
+        result[insert.first!] = 1
+        return result
+    }
+
+    func countLetters(curr: Int, max: Int) -> Counter {
+        print("Current level: \(curr)")
+        if curr == max {
+            return countLocalLetters()
+        }
+        let leftCount = self.left!.countLetters(curr: curr+1, max: max)
+        let rightCount = self.right!.countLetters(curr: curr+1, max: max)
+        return countLocalLetters().merge(leftCount).merge(rightCount)
+    }
+
+}
+
+func calculateResult(_ counts: Counter) -> Int {
+    let counted = quicksort(Array(counts.values))
+    return counted.last! - counted.first!
 }
 
 let polymerTemplate = Array(readLine()!)
@@ -58,11 +75,31 @@ _ = readLine() //empty one
 
 while let input = readLine() {
     let split = input.components(separatedBy: " -> ")
-    rules[split[0]] = split[1]
+    rules[split[0]] = RuleGraph(split[0], split[1])
 }
 
-var ruleGraphs: [RuleGraph] = []
-
-for i in 0..<polymerTemplate.count {
-    ruleGraphs.append(RuleGraph(String(polymerTemplate[i..<i+2]), head: nil))
+for (_, rule) in rules {
+    let names = rule.generate()
+    rule.left = rules[names.0]!
+    rule.right = rules[names.1]!
 }
+
+
+let steps = 40
+var elementCount: Counter = [:]
+for c in polymerTemplate {
+    if let _ = elementCount[c] {
+        elementCount[c]! += 1
+    } else {
+        elementCount[c] = 1
+    }
+}
+for i in 0..<polymerTemplate.count-1 {
+    //print("Count is: \(elementCount)")
+    let pair = String(polymerTemplate[i..<i+2])
+    //print("Processing \(pair)")
+    let head = rules[pair]!
+    elementCount = elementCount.merge(head.countLetters(curr: 1, max: steps))
+}
+
+print("Result: \(calculateResult(elementCount))")
