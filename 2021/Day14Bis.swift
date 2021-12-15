7622d3fc1db44e380fd9551583778526eb87167c
Hombrenieve <hombrenieve@gmail.com>
2021-12-15 23:32:50 +0100

    Day14 finally!

--- 2021/Day14Bis.swift
+++ 2021/Day14Bis.swift
@@ -11,8 +11,7 @@ func quicksort<T: Comparable>(_ a: [T]) -> [T] {
     return quicksort(less) + equal + quicksort(greater)
 }
 
-typealias Counter = Dictionary<Character, Int>
-var rules: Dictionary<String, RuleGraph> = [:]
+typealias Counter = Dictionary<String, Int>
 
 extension Counter {
     func merge(_ rhs: Counter) -> Counter {
@@ -25,6 +24,22 @@ extension Counter {
             }
         }
         return result
+    }
+}
+
+typealias Rules = Dictionary<String, RuleGraph>
+
+extension Rules {
+    func countPairs(_ prev: Counter) -> Counter {
+        var counter: Counter = [:]
+        for (k,_) in self {
+            counter[k] = 0
+        }
+        for (_, r) in self {
+            counter[r.left!.pair]! += prev[r.pair]!
+            counter[r.right!.pair]! += prev[r.pair]!
+        }
+        return counter
     }
 }
 
@@ -46,37 +61,49 @@ class RuleGraph: Equatable {
     func generate() -> (String, String) {
         return (String(pair.first!)+insert, insert+String(pair.last!))
     }
+}
 
-    private func countLocalLetters() -> Counter {
-        var result: Counter = [:]
-        result[insert.first!] = 1
-        return result
-    }
-
-    func countLetters(curr: Int, max: Int) -> Counter {
-        print("Current level: \(curr)")
-        if curr == max {
-            return countLocalLetters()
+func countLetters(fromPairs: Counter) -> Counter {
+    var letterCounter: Counter = [:]
+    for (k, c) in fromPairs {
+        let key = Array(k)
+        for i in 0...1 {
+            let letter = String(key[i])
+            if let exist = letterCounter[letter] {
+                letterCounter[letter] = exist+c
+            } else {
+                letterCounter[letter] = c
+            }
         }
-        let leftCount = self.left!.countLetters(curr: curr+1, max: max)
-        let rightCount = self.right!.countLetters(curr: curr+1, max: max)
-        return countLocalLetters().merge(leftCount).merge(rightCount)
     }
-
+    return letterCounter
 }
 
-func calculateResult(_ counts: Counter) -> Int {
-    let counted = quicksort(Array(counts.values))
-    return counted.last! - counted.first!
+func calcResult(template: String, lettersFromPairs: Counter) -> Int {
+    var count = lettersFromPairs
+    count[String(template.first!)]! -= 1
+    count[String(template.last!)]! -= 1
+    count.forEach({ count[$0] = $1/2 })
+    count[String(template.first!)]! += 1
+    count[String(template.last!)]! += 1
+    let ordered = quicksort(Array(count.values))
+    return ordered.last! - ordered.first!
 }
+
+
 
 let polymerTemplate = Array(readLine()!)
 _ = readLine() //empty one
+
+var rules: Rules = [:]
+var pairCounts: Counter = [:]
 
 while let input = readLine() {
     let split = input.components(separatedBy: " -> ")
     rules[split[0]] = RuleGraph(split[0], split[1])
 }
+
+
 
 for (_, rule) in rules {
     let names = rule.generate()
@@ -84,22 +111,24 @@ for (_, rule) in rules {
     rule.right = rules[names.1]!
 }
 
-
-let steps = 40
-var elementCount: Counter = [:]
-for c in polymerTemplate {
-    if let _ = elementCount[c] {
-        elementCount[c]! += 1
-    } else {
-        elementCount[c] = 1
-    }
+let steps = 10
+//Create counts
+for (k,_) in rules {
+    pairCounts[k] = 0
 }
+
+//Starting pair counts
 for i in 0..<polymerTemplate.count-1 {
-    //print("Count is: \(elementCount)")
     let pair = String(polymerTemplate[i..<i+2])
-    //print("Processing \(pair)")
-    let head = rules[pair]!
-    elementCount = elementCount.merge(head.countLetters(curr: 1, max: steps))
+    pairCounts[pair]! += 1
 }
 
-print("Result: \(calculateResult(elementCount))")
+for _ in 1...steps {
+    pairCounts = rules.countPairs(pairCounts)
+}
+
+
+//print("Pair count: \(pairCounts)")
+let countFromPairs = countLetters(fromPairs: pairCounts)
+//print("Count: \(countFromPairs)")
+print("Result: \(calcResult(template: String(polymerTemplate), lettersFromPairs: countFromPairs))")
