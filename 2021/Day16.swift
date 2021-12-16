7ea7e3cb403e874bd2654c84be2bfd713544d3c4
Hombrenieve <hombrenieve@gmail.com>
2021-12-16 23:21:32 +0100

    Day16 part 1

--- 2021/Day16.swift
+++ 2021/Day16.swift
@@ -11,22 +11,94 @@ extension String {
     }
 }
 
+struct Message {
+    let data: [Character]
+    var currentBit = 0
+    var packets: [Packet] = []
+
+
+    init(_ msg: String) {
+        self.data = Array(msg)
+        while let p = buildPacket() {
+            packets.append(p)
+        }
+    }
+
+    private mutating func buildLiteral(_ v: Int, _ t: Int) -> Packet {
+        var reading = true
+        var numberString = ""
+        while reading {
+            if data[currentBit].wholeNumberValue! == 0 {
+                reading = false
+            }
+            numberString.append(String(data[currentBit+1...currentBit+4]))
+            currentBit += 5
+        }
+        let number = Int(numberString, radix: 2)!
+        let p = Packet(v, t)
+        p.literal = number
+        return p
+    }
+
+    private mutating func buildOperator(_ v: Int, _ t: Int) -> Packet {
+        let i = data[currentBit].wholeNumberValue!
+        currentBit += 1
+        if i == 0 {
+            let length = Int(String(data[currentBit...currentBit+14]), radix: 2)!
+            currentBit += 15
+            let origin = currentBit
+            let p = Packet(v, t)
+            while length != currentBit-origin {
+                p.subPackets.append(buildPacket()!)
+            }
+            return p
+        } else {
+            let count = Int(String(data[currentBit...currentBit+10]), radix: 2)!
+            currentBit += 11
+            let p = Packet(v, t)
+            for _ in 1...count {
+                p.subPackets.append(buildPacket()!)
+            }
+            return p
+        }        
+    }
+
+    private mutating func buildPacket() -> Packet? {
+        if data.count-currentBit < 11 {
+            return nil
+        }
+        let v = Int(String(data[currentBit...currentBit+2]), radix: 2)!
+        let t = Int(String(data[currentBit+3...currentBit+5]), radix: 2)!
+        currentBit += 6
+        switch(t) {
+            case 4:
+                return buildLiteral(v, t)
+            default:
+                return buildOperator(v, t)
+        }
+    }
+}
+
 
 class Packet {
     let version: Int
     let type: Int
-    let literal: Int?
-    let next: Packet?
-    let subPackets: [Packet]
+    var literal: Int? = nil
+    var subPackets: [Packet] = []
 
-    init(binary: String) {
-        let asArray = Array(binary)
-        self.version = Int(String(asArray[0...2]), radix: 2)!
-        self.type = Int(String(asArray[2...4]), radix: 2)!
-        if self.type == 4 {
-            var readingLiteral
-        }
+    init(_ v: Int, _ t: Int) {
+        self.version = v
+        self.type = t        
     }
+}
+
+func sumV(accum: Int, packets: [Packet]) -> Int {
+    if packets.count == 0 {
+        return accum
+    }
+    var local = accum+packets[0].version
+    local += sumV(accum: 0, packets: packets[0].subPackets)
+    return sumV(accum: local, packets: Array(packets.dropFirst()))
 }
 
 func convertToBinary(hex: String) -> String {
@@ -38,6 +110,8 @@ func convertToBinary(hex: String) -> String {
 }
 
 
-let bs = convertToBinary(hex: readLine()!)
-let pk = Packet(binary: bs)
-   
+let msg = Message(convertToBinary(hex: readLine()!))
+
+let sum = sumV(accum: 0, packets: msg.packets)
+
+print("Result: \(sum)")
