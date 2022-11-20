46ce6c0f9ca1b6d097d0bbd694ca6b28b14704c8
Hombrenieve <hombrenieve@gmail.com>
2022-11-20 23:49:12 +0100

    Implement 2018 day1
    To test just implemented day1 of 2018
    in the file test.rs

--- 2022/src/bin/test.rs
+++ 2022/src/bin/test.rs
@@ -1,12 +1,25 @@
 #[path="../common.rs"]
 mod common;
 
-fn calc_freq(changes: Vec<i32>) -> i32 {
+fn calc_freq(changes: &Vec<i32>) -> i32 {
     changes.iter().sum()
+}
+
+fn get_repeated_freq(changes: &Vec<i32>) -> i32 {
+    let mut visited = std::collections::HashSet::new();
+    let mut current = 0;
+    loop {
+        for change in changes {
+            if !visited.insert(current) {
+                return current;
+            }
+            current += change;
+        }
+    }
 }
 
 fn main() {
     let values = common::read_until_eof();
     let changes = common::to_ints(values.as_str(), " ");
-    println!("Freqs are {}", calc_freq(changes));
+    println!("First repeated is {}", get_repeated_freq(&changes));
 }
