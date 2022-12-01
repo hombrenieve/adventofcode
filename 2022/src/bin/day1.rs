ada5be39e8bf748e300466a98ab2edd8c361e3d3
Hombrenieve <hombrenieve@gmail.com>
2022-12-01 07:50:22 +0100

    day1: part 2

--- 2022/src/bin/day1.rs
+++ 2022/src/bin/day1.rs
@@ -6,6 +6,12 @@ fn find_max(rations: &Vec<Vec<i32>>) -> i32 {
     rations.iter().map(|v| v.iter().sum()).max().unwrap()
 }
 
+fn find_top_three_sum(rations: &Vec<Vec<i32>>) -> i32 {
+    let mut sums = rations.iter().map(|v| v.iter().sum()).collect::<Vec<i32>>();
+    sums.sort();
+    sums.iter().rev().take(3).sum()
+}
+
 
 fn main() {
     let mut rations = Vec::new();
@@ -14,5 +20,5 @@ fn main() {
         if strrat.is_empty() { break; }
         rations.push(common::to_ints(&strrat[..], " "));
     }
-    println!("Max: {}", find_max(&rations));
+    println!("Top three sum: {}", find_top_three_sum(&rations));
 }
