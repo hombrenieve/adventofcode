0d41bd7efdad55b27a905589327db3be177ff7a5
Hombrenieve <hombrenieve@gmail.com>
2022-12-12 14:51:04 +0100

    day 12: part 2

--- 2022/src/bin/day12.rs
+++ 2022/src/bin/day12.rs
@@ -70,7 +70,19 @@ fn min_distance(graph: &HashMap<Position, Vertex>) -> Option<Position> {
     Some(filtered[0].0.to_owned())
 }
 
-fn dijkstra(elevation_map: &Vec<Vec<u32>>, start: &Position, end: &Position) -> usize {
+fn best_start_path(graph: &HashMap<Position, Vertex>, elevation_map: &Vec<Vec<u32>>) -> usize {
+    let mut starting_positions = Vec::new();
+    for i in 0..elevation_map.len() {
+        for j in 0..elevation_map[i].len() {
+            if elevation_map[i][j] == 0 {
+                starting_positions.push(Position::new(j as i32, i as i32));
+            }
+        }
+    }
+    graph.iter().filter(|vp| starting_positions.contains(vp.0)).map(|vp| vp.1.distance).min().unwrap()
+}
+
+fn dijkstra(elevation_map: &Vec<Vec<u32>>, end: &Position) -> usize {
     let mut q = HashMap::new();
     let mut pos = Some(end.to_owned());
     for i in 0..elevation_map.len() {
@@ -96,27 +108,22 @@ fn dijkstra(elevation_map: &Vec<Vec<u32>>, start: &Position, end: &Position) -> 
             let possible = current_pos.to_owned() + d.dir_vector().to_owned();
             if can_move(elevation_map, &current_pos, &possible) {
                 if let Some(v) = q.get_mut(&possible) {
-                    if !v.visited && v.distance > u.distance+1 {
+                    if !v.visited && u.distance < usize::max_value() && v.distance > u.distance+1 {
                         v.distance = u.distance + 1;
                     }
                 }
             }
         }
-        if current_pos == start.to_owned() { 
-            q.insert(current_pos, u);
-            break;
-        }
         q.insert(current_pos, u);
         pos = min_distance(&q);
     }
-    q.get(start).unwrap().distance
+    best_start_path(&q, elevation_map)
 }
 
 fn main() {
     let mut elevation_map = Vec::new();
     let mut x = 0;
     let mut y = 0;
-    let mut starting = Position::new(0, 0);
     let mut ending = Position::new(0, 0);
     while let Some(line) = common::read_line() {
         elevation_map.push(
@@ -125,10 +132,7 @@ fn main() {
                     x += 1;
                     match c as u32 {
                         97..=122 => (c as u32) - 97,
-                        83 => {
-                            starting = Position::new(x - 1, y);
-                            0
-                        }
+                        83 => 0,
                         69 => {
                             ending = Position::new(x - 1, y);
                             25
@@ -141,5 +145,5 @@ fn main() {
         y += 1;
         x = 0;
     }
-    println!("Steps: {}", dijkstra(&elevation_map, &starting, &ending));
+    println!("Steps: {}", dijkstra(&elevation_map, &ending));
 }
