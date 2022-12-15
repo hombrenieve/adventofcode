b63006df909a5c0e4fc9797af55515c37bec808e
Hombrenieve <hombrenieve@gmail.com>
2022-12-15 13:33:49 +0100

    day 15: part2
    Re-designed

--- 2022/src/bin/day15.rs
+++ 2022/src/bin/day15.rs
@@ -1,8 +1,11 @@
-use std::collections::HashSet;
+use std::{collections::{HashSet, btree_set::Intersection}, cmp::min_by};
 
 #[path="../common.rs"]
 mod common;
 
+const MIN: i64 = 0;
+const MAX: i64 = 4000000;
+//const MAX: i64 = 20;
 
 #[derive(Debug, Clone, Eq, Hash)]
 struct Point {
@@ -36,39 +39,73 @@ struct Sensor {
     beacon: Point
 }
 
+type Interval = (i64, i64);
+
 impl Sensor {
-    fn exclusion_zone(&self, row: i64) -> Vec<Point> {
-        let mut excl = Vec::new();
+    fn exclusion_interval(&self, row: i64) -> Option<Interval> {
         let md = self.position.manhattan(&self.beacon);
         let mid = Point{ x: self.position.x, y: row};
+        let midmd = self.position.manhattan(&mid);
         if self.position.manhattan(&mid) <= md {
-            excl.push(mid);
-            let mut delta = 1;
-            loop {
-                let pr = Point { x: self.position.x+delta, y: row};
-                let pl = Point { x: self.position.x-delta, y: row};
-                if self.position.manhattan(&pr) <= md {
-                    excl.push(pr); excl.push(pl);
-                } else {
-                    break;
-                }
-                delta += 1;
-            }
+            let delta = md - midmd;
+            return Some((std::cmp::max(self.position.x-delta, MIN), std::cmp::min(self.position.x+delta, MAX)));
         }
-        //Remove beacon
-        if let Some(id) = excl.iter().position(|p| *p == self.beacon) {
-            excl.remove(id);
-        }
-        excl
+        None
     }
 }
 
-fn no_beacon_possible(sensors: &Vec<Sensor>, row: i64) -> usize {
-    let mut exclusions = HashSet::new();
-    for s in sensors {
-        s.exclusion_zone(row).iter().for_each(|p| { exclusions.insert(p.to_owned()); });
+fn superinterval(i1: &Interval, i2: &Interval) -> Option<Interval> {
+    if i2.0 > i1.1 || i1.0 > i2.1 {
+        return None;
     }
-    exclusions.len()
+    Some((std::cmp::min(i1.0, i2.0), std::cmp::max(i1.1, i2.1)))
+}
+
+fn point_in_middle(i1: &Interval, i2: &Interval) -> i64 {
+    if i1.0 < i2.0 {
+        i1.1+1
+    } else {
+        i2.1+1
+    }
+}
+
+fn reduce_intervals(intervals: &Vec<Interval>) -> Vec<Interval> {
+    let mut result = intervals.clone();
+    let mut result1 = Vec::new();
+    loop {
+        let mut interval = result[0];
+        for i in 1..result.len() {
+            if let Some(int) = superinterval(&interval, &result[i]) {
+                interval = int;
+            } else {
+                result1.push(result[i]);
+            }
+        }
+        result1.push(interval);
+        if result.len() == result1.len() {
+            return result1;
+        } else {
+            result = result1;
+            result1 = Vec::new();
+        }
+    }
+    
+}
+
+fn find_beacon(sensors: &Vec<Sensor>) -> Point {
+    for row in MIN..=MAX {
+        let mut ints = Vec::new();
+        sensors.iter().for_each(|s| { 
+            if let Some(int) = s.exclusion_interval(row) {
+                ints.push(int);
+            }
+        });
+        let reduced = reduce_intervals(&ints);
+        if reduced.len() > 1 {
+            return Point{ x: point_in_middle(&reduced[0], &reduced[1]), y: row };
+        }
+    }
+    panic!("Can't find a result");
 }
 
 
@@ -81,5 +118,7 @@ fn main() {
         let mut ps = tokens.split(":").map(|x| Point::from_str(x)).collect::<Vec<Point>>();
         sensors.push(Sensor{position: ps.remove(0), beacon: ps.remove(0)});
     }
-    println!("Exclusions: {}", no_beacon_possible(&sensors, 2000000));
+    let p = find_beacon(&sensors);
+    println!("Point: {:?}", p);
+    println!("Freq: {}", p.x*4000000+p.y);
 }
