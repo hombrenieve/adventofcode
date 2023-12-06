04562e66ddb64875083c8f76fbf8282ef2b37e11
hombrenieve <42200765+hombrenieve@users.noreply.github.com>
2023-12-06 22:50:00 +0000

    day6 part 2
    Thanks to other brain ;-)

--- 2023/day6.cpp
+++ 2023/day6.cpp
@@ -1,8 +1,17 @@
 #include "common.h"
+#include <cmath>
 
 struct race {
-    long time;
-    long distance;
+    long long time;
+    long long distance;
+
+    long long get_max() const {
+        return floor((time*1.0 + std::sqrt(time*time*1.0 - 4.0*distance))/2.0);
+    }
+
+    long long get_min() const {
+        return round((time*1.0 - std::sqrt(time*time*1.0 - 4.0*distance))/2.0);
+    }
 };
 
 race load_race() {
@@ -26,78 +35,9 @@ race load_race() {
     return result;
 }
 
-constexpr int get_distance(int time_holding, int total_time) {
-    return time_holding * (total_time - time_holding);
-}
-
-long find_half(const race& r, long half_size) {
-    std::cout << "Calculating half_size: " << half_size << std::endl;
-    if(half_size >= r.time/2) {
-        return -1;
-    }
-    if(get_distance(r.time/2 - half_size, r.time) > r.distance && get_distance(r.time/2 + half_size, r.time) > r.distance) {
-        long calc = find_half(r, half_size*2);
-        if(calc == -1) {
-            return half_size;
-        } else {
-            return calc;
-        }
-    } else {
-        return -1;
-    }
-}
-
-long find_min(const race&r, long half_size) {
-    if(half_size >= r.time/2) {
-        return 0;
-    }
-    if(get_distance(r.time/2 - half_size, r.time) > r.distance) {
-        //go backwards
-        while(get_distance(r.time/2 - half_size, r.time) > r.distance) {
-            half_size--;
-        }
-        return half_size;
-    } else {
-        //go forwards
-        while(get_distance(r.time/2 - half_size, r.time) > r.distance) {
-            half_size++;
-        }
-        return half_size;
-    }
-}
-
-long find_max(const race&r, long half_size) {
-    if(half_size >= r.time/2) {
-        return r.time/2;
-    }
-    if(get_distance(r.time/2 + half_size - 1, r.time) > r.distance) {
-        //go forwards
-        while(get_distance(r.time/2 + half_size, r.time) > r.distance) {
-            half_size++;
-        }
-        return half_size;
-    } else {
-        //go backwards
-        while(get_distance(r.time/2 + half_size, r.time) > r.distance) {
-            half_size--;
-        }
-        return half_size;
-    }
-}
-
-long find_min_max(const race& r) {
-    std::cout << "time: " << r.time << std::endl;
-    auto half = find_half(r, 1);
-    std::cout << "half: " << half << std::endl;
-    auto min = find_min(r, half);
-    auto max = find_max(r, half);
-    std::cout << "min: " << min << std::endl;
-    std::cout << "max: " << max << std::endl;
-    return max - min -1;
-}
 
 int main() {
     auto race = load_race();
-    auto choices = find_min_max(race);
-    std::cout <<  choices << std::endl;
+    std::cout << "Min is " << race.get_min() << " Max is: " << race.get_max() << std::endl;
+    std::cout <<  "Choices " << race.get_max()-race.get_min() << std::endl;
 }
