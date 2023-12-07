72b9a8ac863a48973d29093679f70acaf6a80b50
hombrenieve <42200765+hombrenieve@users.noreply.github.com>
2023-12-07 17:35:08 +0000

    day 5 part 2 FINISHED
    The input needed one extra line!
    The intervals were really one number shorter
    so I missed the real result by one

--- 2023/day5.cpp
+++ 2023/day5.cpp
@@ -6,7 +6,7 @@ struct interval {
     long min;
     long max;
 
-    interval(long min, long steps) : min(min), max(min+steps-1) {}
+    interval(long min, long steps) : min(min), max(min+steps) {}
 
     long size() const {
         return max - min;
@@ -46,25 +46,19 @@ struct range_map {
                 new_sources.push_back(s);
                 continue;
             }
-            interval i_mappeable(0, 0), i_unmappeable(0, 0);
-            i_mappeable.min = s.min;
-            i_mappeable.max = s.max;
+            interval i_mappeable(s.min, s.size());
             if(s.min < source.min) {
-                i_unmappeable.min = s.min;
-                i_unmappeable.max = source.min - 1;
-                new_sources.push_back(i_unmappeable);
+                new_sources.emplace_back(s.min, source.min - s.min);
                 i_mappeable.min = source.min;
             }
             if(s.max > source.max) {
-                i_unmappeable.min = source.max + 1;
-                i_unmappeable.max = s.max;
-                new_sources.push_back(i_unmappeable);
+                new_sources.emplace_back(source.max + 1, s.max - source.max - 1);
                 i_mappeable.max = source.max;
             }
             mappeable.push_back(i_mappeable);
         }
         for(const auto& m: mappeable) {
-            mapped.emplace_back(m.min - source.min + dest.min, m.size()+1);
+            mapped.emplace_back(m.min - source.min + dest.min, m.size());
         }
         sources = new_sources;
     }
@@ -79,7 +73,6 @@ struct mapping {
         std::vector<interval> sources = intervals;
         std::vector<interval> mapped;
         for(const auto& r: ranges) {
-            std::cout << "Applying " << r.source << " -> " << r.dest << " on " << sources << std::endl;
             r.apply(sources, mapped);
         }
         mapped.insert(mapped.end(), sources.begin(), sources.end());
@@ -94,12 +87,13 @@ std::vector<interval> load_seeds() {
         auto tokens = split(line, ":");
         auto numbers = split_with_rep(tokens[1], ' ');
         for(int i = 0; i < numbers.size(); i+=2) {
-            seeds.emplace_back(std::stol(numbers[i]), std::stol(numbers[i + 1]));
+            seeds.emplace_back(std::stol(numbers[i]), std::stol(numbers[i + 1])-1);
         }
     });
     return seeds;
 }
 
+//WARNING!!! INPUT NEEDS ONE EMPTY LINE AT THE END
 std::vector<mapping> load_mappings() {
     std::vector<mapping> mappings;
     mapping m;
@@ -115,7 +109,7 @@ std::vector<mapping> load_mappings() {
             return;
         }
         auto numbers = split_with_rep(line, ' ');
-        m.ranges.emplace_back(std::stol(numbers[1]), std::stol(numbers[0]), std::stol(numbers[2]));
+        m.ranges.emplace_back(std::stol(numbers[1]), std::stol(numbers[0]), std::stol(numbers[2])-1);
     });
     return mappings;
 }
@@ -125,18 +119,15 @@ long find_min(const std::vector<interval>& seeds, const std::vector<mapping>& ma
     for(const auto& m: mappings) {
         intervals = m.apply(intervals);
     }
-    long min = LONG_MAX;
-    for(const auto& i: intervals) {
-        if(i.min < min) {
-            min = i.min;
-        }
-    }
-    return min;
+    return std::min_element(intervals.begin(), intervals.end(), [](const interval& a, const interval& b) {
+        return a.min < b.min;
+    })->min;
 }
 
 int main() {
     auto seeds = load_seeds();
     auto mappings = load_mappings();
-    std::cout << find_min(seeds, mappings) << std::endl;
+    auto min = find_min(seeds, mappings);
+    std::cout << min << std::endl;
     return 0;
 }
