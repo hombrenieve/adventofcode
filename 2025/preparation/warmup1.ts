bc7ed356d26feb4f2dba98e8e8bc306da7a532a7
Hombrenieve <hombrenieve@gmail.com>
2025-11-07 16:46:17 +0100

    Add warmup folder
    It can be used the same way:
    npx ts-node preparation/warmup1.ts

---
+++ 2025/preparation/warmup1.ts
@@ -0,0 +1,8 @@
+import { readInput } from '../utils';
+
+console.log("Warmup Exercise 1");
+
+const lines = readInput();
+console.log(`Read ${lines.length} lines from input.txt`);
+
+// Your warmup solution here
