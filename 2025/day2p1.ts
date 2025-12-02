c7e5cc059d859bae94888017f8545ebfa4df73b1
Hombrenieve <hombrenieve@gmail.com>
2025-12-02 11:06:58 +0100

    Day2 part 1

--- 2025/day2p1.ts
+++ 2025/day2p1.ts
@@ -2,8 +2,33 @@ import { readInput } from './utils';
 
 console.log("Day 2 - Part 1");
 
-const lines = readInput('inputEx.txt');
+const lines = readInput('inputOf.txt');
+const ranges = lines[0].split(',');
 // const lines = readInput('inputOf.txt');
 console.log(`Read ${lines.length} lines from input file`);
 
-// Your solution here
+function isValidId(id: string, pre: string): boolean {
+    if(id.length === 0) return true;
+    if(id === pre) return false;
+    const current = pre + id[0];
+    return isValidId(id.slice(1), current);
+}
+
+function getRange(range: string): number[] {
+    return range.split('-').map(Number);
+}
+
+let invalidIds: number[] = [];
+
+for (const range of ranges) {
+    const [start, end] = getRange(range);
+    for (let id = start; id <= end; id++) {
+        if (!isValidId(id.toString(), '')) {
+            invalidIds.push(id);
+        }
+    }
+}
+
+// add all invalid IDs
+const sumInvalidIds = invalidIds.reduce((a, b) => a + b, 0);
+console.log(`Sum of invalid IDs: ${sumInvalidIds}`);
